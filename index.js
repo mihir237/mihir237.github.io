b197da6f661d5cb692710b11e64d2701d2658aa9
mihir237 <mihir23701@gmail.com>
2022-05-30 19:06:26 +0530

    commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const showMenu =()=>{
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
+
+const showMenu = () => {
     var element = document.getElementById("menu-sm");
     element.classList.toggle("hidden");
     // element.classList.toggle("text-3xl");
-
-   
-
 }
