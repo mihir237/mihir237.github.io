3ea6c2faffc4e5b3166852db7f1dcd2d35d53376
mihir237 <mihir23701@gmail.com>
2022-06-01 18:33:31 +0530

    resume

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+AOS.init();
+
 window.onbeforeunload = function () {
+
     window.scrollTo(0, 0);
+
 }
 
 const showMenu = () => {
     var element = document.getElementById("menu-sm");
     element.classList.toggle("hidden");
     // element.classList.toggle("text-3xl");
+}
+
+
+document.onreadystatechange = function () {
+    element = document.getElementById("loader");
+    ele = document.getElementById("content")
+    if (document.readyState !== "complete") {
+        ele.classList.toggle("hidden")
+    } else {
+        element.classList.toggle("hidden")
+
+    }
 }
